de9a3b345f66dd8371b68849ec8358a2998e1c3a
Joey Villa <joey@avalar.tech>
2021-06-14 11:25:59 -0400

    pass is now bytea, index boilerplate accounts for screen size

--- RATCH/database/create_db.sh
+++ RATCH/database/create_db.sh
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS Users (
   last_name VARCHAR (32) NOT NULL,
   email VARCHAR (64) UNIQUE NOT NULL,
   username VARCHAR (32) UNIQUE NOT NULL,
-  password VARCHAR (128) NOT NULL,
+  password BYTEA NOT NULL,
   created TIMESTAMP,
   lastseen TIMESTAMP,
   notes TEXT
